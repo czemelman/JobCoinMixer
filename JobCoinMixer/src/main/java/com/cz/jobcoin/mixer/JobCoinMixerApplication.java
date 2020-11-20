83f6b80180235a8aff0ca496ff47b72f3e7bb9d2
Constantin Zemelman <czemelman@gmail.com>
2020-11-20 10:06:58 -0500

    initial commit

---
+++ JobCoinMixer/src/main/java/com/cz/jobcoin/mixer/JobCoinMixerApplication.java
@@ -0,0 +1,13 @@
+package com.cz.jobcoin.mixer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobCoinMixerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobCoinMixerApplication.class, args);
+	}
+
+}
---
+++ JobCoinMixer/src/test/java/com/cz/jobcoin/mixer/JobCoinMixerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cz.jobcoin.mixer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobCoinMixerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
